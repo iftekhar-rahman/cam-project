224f6868c07996a711a42aebefd062fa3306f48a
Iftekhar Rahman <iftekharmail1@gmail.com>
2020-09-09 14:12:49 +0600

    dd

--- js/main.js
+++ js/main.js
@@ -16,50 +16,70 @@ jQuery(document).ready(function($){
 	$(document).on('click', 'span.close-btn', function () {
 		$($(this)[0].previousElementSibling.previousElementSibling).css('background-image', '');
 		$(this).remove();
-	});
+    });
+
+    // const slider = $(".featured-carousel");
+    $('.featured-carousel, .random-carousel-1, .random-carousel-2, .random-carousel-3').slick({
+        slidesToShow: 3,
+        dots:false,
+        arrows: false,
+        easing: 'linear',
+        infinite: true,
+        swipeToSlide: true
+    });
+   
+       
+    
+    // var slider = tns({
+    //     container: '#base',
+    //     items: 3,
+    //     slideBy: 'page',
+    //     autoplay: true
+    //   });
+    
 
     // featured-carousel
-    $('.featured-carousel').owlCarousel({
-        items: 3,
-        loop: false,
-        nav: false,
-        dots: false,
-        autoplay: false,
-        margin: 20,
-    });
-    $('.random-carousel-1').owlCarousel({
-        items: 3,
-        loop: false,
-        nav: false,
-        dots: false,
-        autoplay: false,
-        margin: 15,
-    });
-    $('.random-carousel-2').owlCarousel({
-        items: 3,
-        loop: false,
-        nav: false,
-        dots: false,
-        autoplay: false,
-        margin: 15,
-    });
-    $('.random-carousel-3').owlCarousel({
-        items: 4,
-        loop: false,
-        nav: false,
-        dots: false,
-        autoplay: false,
-        margin: 5,
-        autoWidth: true,
-    });
-    $('.random-carousel-4').owlCarousel({
-        items: 3,
-        loop: false,
-        nav: false,
-        dots: false,
-        autoplay: false,
-        margin: 15,
-    });
+    // $('.featured-carousel').owlCarousel({
+    //     items: 3,
+    //     loop: false,
+    //     nav: false,
+    //     dots: false,
+    //     autoplay: false,
+    //     margin: 20,
+    // });
+    // $('.random-carousel-1').owlCarousel({
+    //     items: 3,
+    //     loop: false,
+    //     nav: false,
+    //     dots: false,
+    //     autoplay: false,
+    //     margin: 15,
+    // });
+    // $('.random-carousel-2').owlCarousel({
+    //     items: 3,
+    //     loop: false,
+    //     nav: false,
+    //     dots: false,
+    //     autoplay: false,
+    //     margin: 15,
+    // });
+    // $('.random-carousel-3').owlCarousel({
+    //     items: 4,
+    //     loop: false,
+    //     nav: false,
+    //     dots: false,
+    //     autoplay: false,
+    //     margin: 5,
+    //     autoWidth: true,
+    // });
+    // $('.random-carousel-4').owlCarousel({
+    //     items: 3,
+    //     loop: false,
+    //     nav: false,
+    //     dots: false,
+    //     autoplay: false,
+    //     margin: 15,
+    // });
 
     
 
