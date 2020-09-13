f1c37bf88a4001693933fde63507bba0b6355dfa
Iftekhar Rahman <iftekharmail1@gmail.com>
2020-09-13 18:29:02 +0600

    ddd

--- js/main.js
+++ js/main.js
@@ -17,44 +17,6 @@ jQuery(document).ready(function($){
       $(this).remove();
     });
 
-    // carousel js
-    // $('.featured-carousel, .random-carousel-1, .random-carousel-2').slick({
-    //     slidesToShow: 3,
-    //     dots:false,
-    //     arrows: false,
-    //     easing: 'linear',
-    //     infinite: true,
-    //     swipeToSlide: true
-    // });
-    // carousel js
-    // $('.random-carousel-3').slick({
-    //     slidesToShow: 3,
-    //     dots:false,
-    //     variableWidth: true,
-    //     arrows: false,
-    //     easing: 'linear',
-    //     infinite: true,
-    //     swipeToSlide: true
-    // });
-
-    // JS for range slider
-    // var p = document.getElementById("rrange"),
-    // res = document.getElementById("percent");
-
-    // p.addEventListener("input", function() {
-    //   res.innerHTML = p.value + "%";
-    //   $('#percent').css('left', p.value * 3.7 - 15);
-    //   $('.value_ident').css('width', p.value*4);
-    // }, false);
-    // $('.rang').on({
-    //     mousedown: function () {
-    //         $('#percent').addClass('active');
-    //     },
-    //     mouseup: function () {
-    //         $('#percent').removeClass('active');
-    //     }
-    // });
-
     // Tab JS
     $('.tabs a').click(function(){
       $('.panel').hide();
@@ -84,35 +46,22 @@ jQuery(document).ready(function($){
       $(this).addClass("active");
     });
 
-    // $(".font-change-btn").on("click", function(){
-    //   $(".font-change, .site-wrap").removeClass("font-active");
-    //   $(this).addClass("font-active");
-    // });
+    // JS for font selection section
     $(".font-change-btn").on("click", function(){
-      $(".font-change, .site-wrap").toggleClass("active");
+      $(".font-change").toggleClass("active");
     });
-    $(".site-wrap.active").on("click", function(){
-      $(".font-change, .site-wrap").removeClass("active");
+    $(".font-change-btn").on("click", function(){
+      $(".overlay").toggleClass("active");
     });
-    // $(".font-change-btn").on("click", function(){
-    //   $(".font-change, .site-wrap").removeClass("active");
-    // });
-    // js for redo-undo-btns
-    // $(".font-change-btn").on("click", function(){
-    //   $(".font-change").toggleClass("active");
-    // });
+    $(".overlay").on("click", function(){
+      $(".overlay, .font-change").removeClass("active");
+    });
 
-
-
-
-
-
-
-
-
-
-
-
+    // JS for range slider
+    var slider = document.getElementById("slider");
+    slider.oninput = function() {
+      $('.fill').css('width', this.value + '%');
+    }
 
     
 
